f4ccf06557d9cc9b729bb7c99b8f5954808e4275
loezzje <m.demmendaal@hotmail.com>
2017-09-13 12:29:38 +0200

    add correct count

---
+++ test.js
@@ -0,0 +1,27 @@
+// var languageName = 'javascript';
+// document.write(languageName);
+//
+// var age = 32;
+// document.write(age);
+//
+// var a = 2;
+// var b = 3;
+// document.write(a+b);
+
+
+function meditate() {
+  return "ohm";
+};
+
+// console.log(meditate());
+// for (var timer = 0; timer < 25; timer++) {
+//   console.log("tik tak ");
+// };
+var energy = 0;
+if (energy < 10) {
+  console.log(meditate());
+
+  };
+  else {
+    console.log("work");
+  };
